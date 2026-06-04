8108b7889b2708a5cdd09dee4bc7b5be8eaac73c
DiegoSusa <diegosusajimenez@gmail.com>
2026-06-03 22:14:23 -0500

    app.py and application.py changes

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def reservar():
         return {"success": False, "error": str(e)}
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))
--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from app import app
+import os
 
 application = app
 
 if __name__ == "__main__":
-    application.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))
